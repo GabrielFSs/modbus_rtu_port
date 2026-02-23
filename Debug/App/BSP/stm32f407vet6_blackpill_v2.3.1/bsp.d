f3d7a7b2810971ad471e1ab9948a6910f2848261
GabrielFS <gab_ptc@outlook.com>
2026-02-23 06:44:52 -0300

    update modbus-slave (freemodbus) to work with hal_uart and its tx done feature

--- Debug/App/BSP/stm32f407vet6_blackpill_v2.3.1/bsp.d
+++
@@ -1,4 +0,0 @@
-App/BSP/stm32f407vet6_blackpill_v2.3.1/bsp.o: \
- ../App/BSP/stm32f407vet6_blackpill_v2.3.1/bsp.c \
- ../App/BSP/stm32f407vet6_blackpill_v2.3.1/bsp.h
-../App/BSP/stm32f407vet6_blackpill_v2.3.1/bsp.h:
