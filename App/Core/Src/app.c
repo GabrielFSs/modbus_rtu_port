115b9585c4484b0791e570baca84105afb9f4420
GabrielFS <gab_ptc@outlook.com>
2026-03-04 13:55:20 -0300

    adding hal-spi to integrate with toutch interrupt

--- App/Core/Src/app.c
+++ App/Core/Src/app.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include "stm32f4xx_hal.h"
 
-#include "bsp_display_fsmc.h"
-#include "ili9341.h"
+#include <stdint.h>
+#include <stddef.h>
+
 
 
 void app_setup(void)
 {
 
 }
-
-/* ================= APP LOOP ================= */
 
 void app_loop(void)
 {
